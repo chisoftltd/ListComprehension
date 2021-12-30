2bf69f07dac3c17ec7541548d408024b8ff72cb3
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-30 14:17:28 +0000

    find even of a list using List Comprehension

--- main.py
+++ main.py
@@ -19,25 +19,28 @@
 # You are going to write a List Comprehension to create a new list called squared_numbers. This new list should contain
 # every number in the list numbers but each number should be squared.
 
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above 👆
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # 🚨 Do Not Change the code above 👆
+#
+# #Write your 1 line code 👇 below:
+# # squared_numbers = [sqr_number * sqr_number for sqr_number in numbers]
+# squared_numbers = [sqr_number**2 for sqr_number in numbers]
+#
+#
+# #Write your code 👆 above:
+#
+# print(squared_numbers)
 
-#Write your 1 line code 👇 below:
-# squared_numbers = [sqr_number * sqr_number for sqr_number in numbers]
-squared_numbers = [sqr_number**2 for sqr_number in numbers]
 
-
-#Write your code 👆 above:
-
-print(squared_numbers)
-
+# You are going to write a List Comprehension to create a new list called result. This new list should only contain the
+# even numbers from the list numbers.
 
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 # 🚨 Do Not Change the code above
 
 #Write your 1 line code 👇 below:
 
-
+result = [num for num in numbers if num % 2 == 0]
 
 #Write your code 👆 above:
 
