b33ce24e5a62df1a6eafd611f6dd66c80cde6d28
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-30 13:49:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+numbers = [9, 8, 7]
+new_numbers = [n + 1 for n in numbers]
+new_numbers = [n - 1 for n in numbers]
+new_numbers = [n + 1 for n in numbers]
+new_plus_numbers = [n + 1 for n in numbers]
+new_multi_numbers = [n * 1 for n in numbers]
+new_multiple_numbers = [n - 1 for n in numbers]
+new_multi_numbers = [n * 1.1 for n in numbers]
+new_mod_numbers = [n % 1.1 for n in numbers]
+name = "Benjamin"
+letter_list = [letter for letter in name]
+new_range = [letter * 2 for letter in range(0, 5)]
+new_range = [letter * 2 for letter in range(0, 5)]
+names = ['Jon', 'Bill', 'Maria', 'Jenny', 'Jack', "Mark", "Amber", "Todd", "Anita", "Sandy"]
+new_four_names = [name for name in names if len(name) < 5]
+new_four_names = [name for name in names if len(name) < 4]
+new_capital_names = [name.upper() for name in names]
